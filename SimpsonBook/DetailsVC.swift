701a38135417cd791d2cbc9f46fd46371841030f
BakytDzhumabev <djumabaevb@gmail.com>
2021-06-18 21:15:11 +0600

    view 10 rows in table view

--- SimpsonBook/DetailsVC.swift
+++ SimpsonBook/DetailsVC.swift
@@ -11,8 +11,9 @@ class DetailsVC: UIViewController {
 
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var jobLabel: UILabel!
-    
     @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
