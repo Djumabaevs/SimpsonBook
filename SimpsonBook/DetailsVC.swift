09a4487467b6084f1c2e84e066b4dd0fc2f96856
BakytDzhumabev <djumabaevb@gmail.com>
2021-06-18 21:01:56 +0600

    view controller created using cocoa class

---
+++ SimpsonBook/DetailsVC.swift
@@ -0,0 +1,25 @@
+//
+//  DetailsVC.swift
+//  SimpsonBook
+//
+//  Created by Bakyt Dzhumabaev on 18/6/21.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
