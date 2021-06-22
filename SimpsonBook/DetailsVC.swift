8d49a5149a3c8961532e826059c4da8f5d6fa43b
BakytDzhumabev <djumabaevb@gmail.com>
2021-06-23 00:18:20 +0600

    Simpson? optional

--- SimpsonBook/DetailsVC.swift
+++ SimpsonBook/DetailsVC.swift
@@ -13,10 +13,14 @@ class DetailsVC: UIViewController {
     @IBOutlet weak var jobLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var selectedSimpson : Simpson?
+    
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
+        nameLabel.text = selectedSimpson?.name
 
        
     }
