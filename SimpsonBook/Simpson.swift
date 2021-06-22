d71b4dbe317ae6e637853e497eeaed927378e45f
BakytDzhumabev <djumabaevb@gmail.com>
2021-06-23 00:10:27 +0600

    array and count

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,23 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Bakyt Dzhumabaev on 18/6/21.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simsponName: String, simpsonJob: String, simpsonImage: UIImage) {
+        name = simsponName
+        job = simpsonJob
+        image = simpsonImage
+    }
+    
+}
--- SimpsonBook/ViewController.swift
+++ SimpsonBook/ViewController.swift
@@ -11,11 +11,28 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var tableView: UITableView!
     
+    var mySimpsons = [Simpson]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
         tableView.delegate = self
+        
+        let homer = Simpson(simsponName: "Homer", simpsonJob: "Nuclear safety", simpsonImage: UIImage(named: "homer")!)
+        
+        let marge = Simpson(simsponName: "Marge", simpsonJob: "Housewife", simpsonImage: UIImage(named: "marge")!)
+        
+        let bart = Simpson(simsponName: "Bart", simpsonJob: "Student", simpsonImage: UIImage(named: "bart")!)
+        
+        let lisa = Simpson(simsponName: "Lisa", simpsonJob: "Student", simpsonImage: UIImage(named: "lisa")!)
+        
+        let maggie = Simpson(simsponName: "Maggie", simpsonJob: "baby", simpsonImage: UIImage(named: "maggie")!)
+        
+        //let simpsonsArray = [homer, marge, bart, lisa, maggie]
+        
+        
+        
         
     }
     
